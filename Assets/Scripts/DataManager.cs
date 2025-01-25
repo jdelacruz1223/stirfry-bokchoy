c13774bc4265319992680a135a1abf330d55f762
Justin Dela Cruz <delacruz.justin1223@gmail.com>
2025-01-25 14:23:37 -0800

    button functionality

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -5,7 +5,7 @@ public class DataManager : MonoBehaviour
     private static DataManager instance;
     public static DataManager Instance {get {return instance;}}
     
-    [SerializeField] public bool Debug;
+    [SerializeField] public bool DebugMode;
 
     public void Awake()
     {
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,15 +4,23 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     Scene currentScene;
+
+
     public void StartButton()
     {
-        if (DataManager.Instance.Debug) Debug.Log("Start Button");
+        if (DataManager.Instance.DebugMode) Debug.Log("Start Button");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitButton()
     {
-        if (DataManager.Instance.Debug) Debug.Log("Quit Button");
+        if (DataManager.Instance.DebugMode) Debug.Log("Quit Button");
         Application.Quit();
+    }
+
+    public void OptionsButton()
+    {
+        if (DataManager.Instance.DebugMode) Debug.Log("Options Button (Nothing atm)");
+
     }
 }
