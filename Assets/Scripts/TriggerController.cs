cb83ac0f0291f7c31bd818c56ae2cc1e3b6635f1
toonlank <50122169+toonlank@users.noreply.github.com>
2025-01-26 12:54:46 -0800

    Added end Sequence

--- Assets/Scripts/TriggerController.cs
+++ Assets/Scripts/TriggerController.cs
@@ -1,11 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TriggerController : MonoBehaviour
 {
+    [SerializeField] private GameObject endScreen;
+    [SerializeField] private AudioSource fanfare;
+
+    private IEnumerator endSequence() {
+        if(endScreen != null) endScreen.SetActive(true);
+        if (fanfare != null && fanfare.clip != null) fanfare.Play();
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("End");
+    }
     void OnTriggerEnter2D(Collider2D trigger)
     {
         Debug.Log("end");
-        SceneManager.LoadScene("End");
+        StartCoroutine("endSequence");
     }
 }
